8d42eaee620f0762f231cf649da8ce7c7c844422
James Chevalier <jchevalier@gmail.com>
2013-06-03 19:32:27 -0400

    comment out active record config, as this app isn't using active record

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -31,7 +31,7 @@ LaunchSoon::Application.configure do
   config.action_mailer.default_url_options = { :host => 'launchsoon.dev' }
 
   # Raise exception on mass assignment protection for Active Record models
-  config.active_record.mass_assignment_sanitizer = :strict
+  # config.active_record.mass_assignment_sanitizer = :strict
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
