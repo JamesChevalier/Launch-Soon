da7013f795617eb6e9515ac7494ea2f83425edba
James Chevalier <jchevalier@gmail.com>
2013-06-03 22:17:39 -0400

    refactor 'interested' action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,57 +6,42 @@ class HomeController < ApplicationController
 
   def interested
     require 'digest'
-    if params['email'].blank?
-      redirect_to root_url
-    else
-      @userpresent   = false
-      @ref           = params['ref']
-      @email         = params['email']
-      @referral_code = Digest::MD5.hexdigest(@email)
-      begin
-        h            = Hominid::API.new(MAILCHIMP_API_KEY)
-      rescue => e
-        flash[:alert] = "There was a problem connecting to MailChimp"
-        render :index
-      end
+    return redirect_to root_url if params['email'].blank?
+    @ref           = params['ref']
+    email          = params['email']
+    @referral_code = Digest::MD5.hexdigest(email)
+    begin
+      h            = Hominid::API.new(MAILCHIMP_API_KEY)
+    rescue => e
+      flash.now[:alert] = "There was a problem connecting to MailChimp"
+      return render :index
+    end
 
-      unless h.nil?
-        @list          = h.find_list_by_name('Interested')
-        @list_members  = h.list_members(@list['id']) if @list
+    list          = h.find_list_by_name('Interested')
+    list_members  = h.list_members(list['id']) if list
 
-        # Increment RCODE for the referring user
-        unless @ref.blank?
-          @list_members['data'].each do |u| # This would be more efficient if I went 50 at a time
-            member_info = h.list_member_info(@list['id'], {'email' => u['email']}) # This can handle up to 50 emails
-            if member_info['data'][0]['merges']['RCODE'] == @ref
-              referral_count = member_info['data'][0]['merges']['RCOUNT']
-              updated_count = referral_count.to_i + 1
-              h.list_update_member(@list['id'], u['email'], {'RCOUNT' => updated_count})
-              break
-            end
-          end
-        end
-
-        # If @email exists in the list, then render their stats page
-        @list_members['data'].each do |u|
-          if u['email'] == @email
-            returning_member_info = h.list_member_info(@list['id'], {'email' => u['email']})
-            @referral_count       = returning_member_info['data'][0]['merges']['RCOUNT']
-            @referral_code        = returning_member_info['data'][0]['merges']['RCODE']
-            @userpresent          = true
-            render :stats
-            break
-          end
-        end
-
-        # Add the email to Mailchimp, since it wasn't already in the list
-        if @userpresent == false
+    # If email exists in the list, then render their stats page
+    # Otherwise, increment RCODE for the referring user
+    list_members['data'].group_by{|u| u['email']}.keys.in_groups_of(50, false).each do |users|
+      members_info = h.list_member_info(list['id'], users)
+      members_info['data'].each do |member_info|
+        if member_info['email'] == email
+          @referral_count       = member_info['merges']['RCOUNT']
+          @referral_code        = member_info['merges']['RCODE']
+          return render :stats
+          break
+        elsif member_info['merges']['RCODE'] == @ref
+          referral_count = member_info['merges']['RCOUNT']
+          updated_count = referral_count.to_i + 1
+          h.list_update_member(list['id'], member_info['email'], {'RCOUNT' => updated_count})
+          # Add the email to Mailchimp, since it wasn't already in the list
           begin
-            h.list_subscribe(@list['id'], @email, {'RCODE' => @referral_code, 'RCOUNT' => 0}, 'text', false, true, true, false)
-          rescue Hominid::APIError => e
-            @error_message = e
+            h.list_subscribe(list['id'], email, {'RCODE' => @referral_code, 'RCOUNT' => 0}, 'text', false, true, true, false)
+          rescue => e
+            flash.now[:alert] = "There was a problem connecting to MailChimp"
             render :index
           end
+          break
         end
       end
     end
