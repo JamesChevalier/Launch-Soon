497c29fba6e50a717805d0444efcc6cbd4ea684d
James Chevalier <jchevalier@gmail.com>
2013-06-07 12:56:31 -0400

    Fix for problem adding completely new users
    If there was no referral code, users were not being added to MailChimp.
    Also, I can optimize this code…

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,28 +21,36 @@ class HomeController < ApplicationController
     list_members  = h.list_members(list['id']) if list
 
     # If email exists in the list, then render their stats page
-    # Otherwise, increment RCODE for the referring user
+    # Or, if a referral code is present, increment RCODE for the referring user then sign up the new user
+    # Otherwise, just sign up the new user
     list_members['data'].group_by{|u| u['email']}.keys.in_groups_of(50, false).each do |users|
       members_info = h.list_member_info(list['id'], users)
       members_info['data'].each do |member_info|
         if member_info['email'] == email
           @referral_count       = member_info['merges']['RCOUNT']
           @referral_code        = member_info['merges']['RCODE']
-          return render :stats
+          render :stats and return
           break
         elsif member_info['merges']['RCODE'] == @ref
+          # There was a referral, so increment that and add the email to Mailchimp
           referral_count = member_info['merges']['RCOUNT']
           updated_count = referral_count.to_i + 1
           h.list_update_member(list['id'], member_info['email'], {'RCOUNT' => updated_count})
-          # Add the email to Mailchimp, since it wasn't already in the list
           begin
             h.list_subscribe(list['id'], email, {'RCODE' => @referral_code, 'RCOUNT' => 0}, 'text', false, true, true, false)
           rescue => e
-            flash.now[:alert] = "There was a problem connecting to MailChimp"
-            render :index
+            flash.now[:alert] = "There was a problem adding #{email}"
+            render :index and return
           end
           break
         end
+      end
+      # There was no referral, so just add the email to Mailhcimp
+      begin
+        h.list_subscribe(list['id'], email, {'RCODE' => @referral_code, 'RCOUNT' => 0}, 'text', false, true, true, false)
+      rescue => e
+        flash.now[:alert] = "There was a problem adding #{email}"
+        render :index
       end
     end
   end
