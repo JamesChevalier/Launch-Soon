87b3a7f99642210bc7ba01cabe260d33c46980e2
James Chevalier <jchevalier@gmail.com>
2013-09-24 22:20:38 -0400

    WIP improve mailchimp signup
    fixes issue with mailchimp list with no subscribers
    still need to work out random failures

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -23,29 +23,38 @@ class HomeController < ApplicationController
     # If email exists in the list, then render their stats page
     # Or, if a referral code is present, increment RCODE for the referring user then sign up the new user
     # Otherwise, just sign up the new user
-    list_members['data'].group_by{|u| u['email']}.keys.in_groups_of(50, false).each do |users|
-      members_info = h.list_member_info(list['id'], users)
-      members_info['data'].each do |member_info|
-        if member_info['email'] == email
-          @referral_count       = member_info['merges']['RCOUNT']
-          @referral_code        = member_info['merges']['RCODE']
-          render :stats and return
-          break
-        elsif member_info['merges']['RCODE'] == @ref
-          # There was a referral, so increment that and add the email to Mailchimp
-          referral_count = member_info['merges']['RCOUNT']
-          updated_count = referral_count.to_i + 1
-          h.list_update_member(list['id'], member_info['email'], {'RCOUNT' => updated_count})
-          begin
-            h.list_subscribe(list['id'], email, {'RCODE' => @referral_code, 'RCOUNT' => 0}, 'text', false, true, true, false)
-          rescue => e
-            flash.now[:alert] = "There was a problem adding #{email}"
-            render :index and return
+    if list_members['total'] > 0
+      list_members['data'].group_by { |u| u['email'] }.keys.in_groups_of(50, false).each do |users|
+        members_info = h.list_member_info(list['id'], users)
+        members_info['data'].each do |member_info|
+          if member_info['email'] == email
+            @referral_count       = member_info['merges']['RCOUNT']
+            @referral_code        = member_info['merges']['RCODE']
+            render :stats && return
+            break
+          elsif member_info['merges']['RCODE'] == @ref
+            # There was a referral, so increment that and add the email to Mailchimp
+            referral_count = member_info['merges']['RCOUNT']
+            updated_count  = referral_count.to_i + 1
+            h.list_update_member(list['id'], member_info['email'], { 'RCOUNT' => updated_count })
+            begin
+              h.list_subscribe(list['id'], email, { 'RCODE' => @referral_code, 'RCOUNT' => 0 }, 'text', false, true, true, false)
+            rescue => e
+              flash.now[:alert] = "There was a problem adding #{email}"
+              render :index && return
+            end
+            break
           end
-          break
+        end
+        # There was no referral, so just add the email to Mailhcimp
+        begin
+          h.list_subscribe(list['id'], email, { 'RCODE' => @referral_code, 'RCOUNT' => 0 }, 'text', false, true, true, false)
+        rescue => e
+          flash.now[:alert] = "There was a problem adding #{email}"
+          render :index
         end
       end
-      # There was no referral, so just add the email to Mailhcimp
+    else
       begin
         h.list_subscribe(list['id'], email, { 'RCODE' => @referral_code, 'RCOUNT' => 0 }, 'text', false, true, true, false)
       rescue => e
